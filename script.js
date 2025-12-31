c5e2c9efd7d1047b43aa8e0af45ae7a202e8413a
Mishita Gogia <mishita.2502050201@muj.manipal.edu>
2025-12-31 14:06:22 +0530

    commit2

--- script.js
+++ script.js
@@ -29,34 +29,34 @@ function toDoList(task) {
   liEl.innerText = newTask;
   ulEl.appendChild(liEl);
   inputEl.value = "";
-  const checkBtnEl = document.createElement("div");
+  const checkBtnEl = document.createElement("div"); /* Adds placeholder for check icon*/
   checkBtnEl.innerHTML = `
   <i class="fas fa-check-square">
-  `;
-  liEl.appendChild(checkBtnEl);
+  `; /*adds font awesome check icon*/
+  liEl.appendChild(checkBtnEl); /*adds the icon in the placeholder*/
   const trashBtnEl = document.createElement("div");
   trashBtnEl.innerHTML = `
   <i class="fas fa-trash"></i>
   `;
   liEl.appendChild(trashBtnEl);
 
-  checkBtnEl.addEventListener("click", () => {
-    liEl.classList.toggle("checked");
-    updateLocalStorage();
+  checkBtnEl.addEventListener("click", () => { /*runs when check icon is clicked - marks as done*/
+    liEl.classList.toggle("checked"); /*adds or removes the styling*/
+    updateLocalStorage(); /*updates the saved data to local storage*/
   });
-
+/*below portion is same as the above checked one - it is just for trash*/
   trashBtnEl.addEventListener("click", () => {
     liEl.remove();
     updateLocalStorage();
   });
-  updateLocalStorage();
+  updateLocalStorage(); /*function call to save the tasks*/
 }
 
-function updateLocalStorage() {
-  const liEls = document.querySelectorAll("li");
-  list = [];
-  liEls.forEach((liEl) => {
-    list.push({
+function updateLocalStorage() { 
+  const liEls = document.querySelectorAll("li"); /*selects all task items*/
+  list = []; /*clears old data - to save the new one into storage - otherwise overwriting will happen*/
+  liEls.forEach((liEl) => { /*loops through tasks*/
+    list.push({ /*saves tasks and completion status*/
       name: liEl.innerText,
       checked: liEl.classList.contains("checked"),
     });
